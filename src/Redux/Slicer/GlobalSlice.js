9a9dee9a51a2cc9ef13984aab7a273b11a71708b
Hanika <hanikap.brainerhub@gmail.com>
2023-12-18 13:37:48 +0530

    changes in global slice

--- src/Redux/Slicer/GlobalSlice.js
+++ src/Redux/Slicer/GlobalSlice.js
@@ -1,27 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const globalSlice = createSlice({
-  name: 'search',
+  name: 'global',
   initialState: {
     searchQuery: '',
+    priceRange: [0, 1000],
   },
-
-  name: 'price',
-  initialState: {
-    priceRange: [0, 1000], 
-    },
   reducers: {
     setSearchQuery: (state, action) => {
       state.searchQuery = action.payload;
     },
-
-     setPriceRange: (state, action) => {
-        state.priceRange = action.payload;
-      },
-    
-
+    setPriceRange: (state, action) => {
+      state.priceRange = action.payload;
+    },
   },
-})
+});
 
-export const { setSearchQuery ,setPriceRange} = globalSlice.actions;
+export const { setSearchQuery, setPriceRange } = globalSlice.actions;
 export default globalSlice.reducer;
